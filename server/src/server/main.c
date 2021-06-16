e0f3a09d4e991e262be3ed2a79ea39d74cd30f2c
acpopper <acpopper@uc.cl>
2021-06-16 18:26:30 -0400

    compact function

--- server/src/server/main.c
+++ server/src/server/main.c
@@ -35,35 +35,14 @@ void * handle_connection(void *p_client_socket){
       if (action == 0){
         // Rendirse
       }
-      else if (action == 1){
-        // Habilidad 1
+      else{
         // Encuentra las entities y usa la funcion use_ability que modifica los valores de las entidades
         entity* user;
         entity* target;
         user = select_user(client_socket, entities);
-        target = select_target(client_socket, entities, 1, user->type);
+        target = select_target(client_socket, entities, action, user->type);
         printf("User: %s Target: %s\n", user->jugador->nombre, target->type);
         // use_ability(user, target, user->jugador->ability1_name, entities, actual_connections);
-      }
-      else if (action == 2){
-        // Habilidad 2 
-        // Encuentra las entities y usa la funcion use_ability que modifica los valores de las entidades
-        entity* user;
-        entity* target;
-        user = select_user(client_socket, entities);
-        target = select_target(client_socket, entities, 2, user->type);
-        printf("User: %s Target: %s\n", user->jugador->nombre, target->type);
-        // use_ability(user, target, user->jugador->ability2_name, entities, actual_connections);
-      }
-      else if (action == 3){
-        // Habilidad 3
-        // Encuentra las entities y usa la funcion use_ability que modifica los valores de las entidades
-        entity* user;
-        entity* target;
-        user = select_user(client_socket, entities);
-        target = select_target(client_socket, entities, 3, user->type);
-        printf("User: %s Target: %s\n", user->jugador->nombre, target->type);
-        // use_ability(user, target, user->jugador->ability2_name, entities, actual_connections);
       }
     }
     else if(msg_code ==-1){
