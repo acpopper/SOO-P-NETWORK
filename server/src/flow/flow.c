89dee7271ba4de53f84fa78de8349678b126fa1b
Tomas Mellado <timellado@uc.cl>
2021-06-19 18:29:51 -0400

    fix: msg and info bigger, formatting

--- server/src/flow/flow.c
+++ server/src/flow/flow.c
@@ -2,146 +2,189 @@
 #include "../coms/comunication.h"
 #include "../entities/chars.h"
 
+const int MAX_PLAYERS = 4;
 
-void welcome_client(int client_socket){
-    char* welcome_msg = "Bienvenido a Monster Hunter: Ruz";
-    server_send_message(client_socket, 2,welcome_msg);
+void welcome_client(int client_socket)
+{
+    char *welcome_msg = "Bienvenido a Monster Hunter: Ruz";
+    server_send_message(client_socket, 2, welcome_msg);
     //code: 0 -> welcome msg
 }
 
-void add_name(entity** players, int client_socket){
-    char* name = server_receive_payload(client_socket);
-    for(int i =0; i<4; i++){
-        if(players[i]!=0){
-            if(players[i]->jugador->client_socket==client_socket){
+void add_name(entity **players, int client_socket)
+{
+    char *name = server_receive_payload(client_socket);
+    for (int i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (players[i] != 0)
+        {
+            if (players[i]->jugador->client_socket == client_socket)
+            {
                 name_entity_player(players[i], name);
             }
         }
     }
 }
 
-void add_type(entity** players, int client_socket){
+void add_type(entity **players, int client_socket)
+{
     char *msg = server_receive_payload(client_socket);
     int type = atoi(msg);
     free(msg);
     int leader = 0;
-    for(int i =0; i<4; i++){
-        if(players[i]!=0){
-            if(players[i]->jugador->client_socket==client_socket){
+    for (int i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (players[i] != 0)
+        {
+            if (players[i]->jugador->client_socket == client_socket)
+            {
                 type_entity_player(players[i], type);
             }
-            if(players[i]->jugador->party_leader){
-                leader= players[i]->jugador->client_socket;
+            if (players[i]->jugador->party_leader)
+            {
+                leader = players[i]->jugador->client_socket;
             }
-        }   
+        }
     }
-    notify_leader(leader,players, client_socket);
+    notify_leader(leader, players, client_socket);
 }
 
-void notify_leader(int leader, entity** players, int new_player){
+void notify_leader(int leader, entity **players, int new_player)
+{
     char msg[20];
-    for(int i=0; i<4; i++){
-        if(players[i]->jugador->client_socket==new_player){
+    for (int i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (players[i]->jugador->client_socket == new_player)
+        {
             sprintf(msg, "%s - %s \n", players[i]->jugador->nombre, players[i]->type);
             break;
         }
     }
-    server_send_message(leader,3,msg);
+    server_send_message(leader, 3, msg);
 }
 
-bool game(entity** players, int leader,int connections){
+bool game(entity **players, int leader, int connections)
+{
     int count_players_with_name = 0;
     char *msg = server_receive_payload(leader);
     int type = atoi(msg);
     free(msg);
     bool start = false;
-    if(!type){
-        if(connections == 4){
+    if (!type)
+    {
+        if (connections == MAX_PLAYERS)
+        {
             //lider esta obligado a comenzar -> debe elegir monstruo
             printf("hola\n");
-            server_send_message(leader, 4,"1" );
+            server_send_message(leader, 4, "1");
         }
-    } else{
-        for(int i=0; i<4; i++){
-            if(players[i]!=0){
-                if(strcmp(players[i]->jugador->nombre,"")!=0 && strcmp(players[i]->type,"")!=0 ){
+    }
+    else
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (players[i] != 0)
+            {
+                if (strcmp(players[i]->jugador->nombre, "") != 0 && strcmp(players[i]->type, "") != 0)
+                {
                     count_players_with_name++;
                 }
-            } 
+            }
         }
-        if(connections==count_players_with_name){
+        if (connections == count_players_with_name)
+        {
             start = true;
-            players[4] = new_monster(type);
-        
-            for(int i=0; i<4;i++){
-                if(players[i]!=0){
-                    server_send_message(players[i]->jugador->client_socket,5,players[4]->type);
+            players[MAX_PLAYERS] = new_monster(type);
+
+            for (int i = 0; i < MAX_PLAYERS; i++)
+            {
+                if (players[i] != 0)
+                {
+                    server_send_message(players[i]->jugador->client_socket, 5, players[MAX_PLAYERS]->jugador->nombre);
                 }
             }
-
-        }  else{
+        }
+        else
+        {
             //error: no todos los jugadores han ingresado su nombre
-            server_send_message(leader,4,"0");
-        } 
+            server_send_message(leader, 4, "0");
+        }
     }
-   
 
     return start;
 }
 
-void turno(entity** players, int client_socket){
+void turno(entity **players, int client_socket)
+{
     int turno_actual = client_socket;
     print_situacion(turno_actual, players);
-    for(int i=0; i<4; i++){
-        if(players[i]->jugador->client_socket == client_socket){
-            if(strcmp(players[i]->type, "Cazador") == 0){
-                server_send_message(turno_actual,7,"");
+    for (int i = 0; i < 4; i++)
+    {
+        if (players[i]->jugador->client_socket == client_socket)
+        {
+            if (strcmp(players[i]->type, "Cazador") == 0)
+            {
+                server_send_message(turno_actual, 7, "");
             }
-            else if (strcmp(players[i]->type, "Médico") == 0){
-                server_send_message(turno_actual,8,"");
+            else if (strcmp(players[i]->type, "Médico") == 0)
+            {
+                server_send_message(turno_actual, 8, "");
             }
-            else if (strcmp(players[i]->type, "Hacker") == 0){
-                server_send_message(turno_actual,9,"");
+            else if (strcmp(players[i]->type, "Hacker") == 0)
+            {
+                server_send_message(turno_actual, 9, "");
             }
             break;
         }
     }
 }
 
-void print_situacion(int client_socket, entity** players){
-    char msg[100];
-    char info[50];
-    for(int i=0; i<5; i++){
-        if (players[i]!= 0){
-            if (players[i]->is_player){
+void print_situacion(int client_socket, entity **players)
+{
+    char msg[200];
+    char info[100];
+    for (int i = 0; i < 5; i++)
+    {
+        if (players[i] != 0)
+        {
+            if (players[i]->is_player)
+            {
                 sprintf(info, "[%s] %s -> VIDA: %d / %d\n", players[i]->jugador->nombre, players[i]->type, players[i]->vida, players[i]->vida_max);
                 strcat(msg, info);
             }
-            else{
+            else
+            {
                 sprintf(info, "[MONSTER] %s -> VIDA: %d / %d\n", players[i]->type, players[i]->vida, players[i]->vida_max);
                 strcat(msg, info);
             }
-        } 
+        }
     }
-    server_send_message(client_socket,6,msg);
+    server_send_message(client_socket, 6, msg);
 }
 
-entity* select_user(int client_socket, entity** entities){
-    entity* user;
-    for(int i=0; i<5; i++){
-        if(entities[i] && entities[i]->is_player && entities[i]->jugador->client_socket==client_socket){
+entity *select_user(int client_socket, entity **entities)
+{
+    entity *user;
+    for (int i = 0; i < 5; i++)
+    {
+        if (entities[i] && entities[i]->is_player && entities[i]->jugador->client_socket == client_socket)
+        {
             user = entities[i];
         }
     }
     return user;
 }
 
-entity* select_target(int client_socket, entity** entities, int skill, char* type_user){
-    entity* target;
+entity *select_target(int client_socket, entity **entities, int skill, char *type_user)
+{
+    entity *target;
     // Si no hay opcion de elegir (skills 2 o 3 para cualquiera) o si es cazador, target=monstruo
-    if(skill>1 || !strcmp(type_user, "Cazador")){
-        for(int i=0; i<5; i++){
-            if(entities[i] && !(entities[i]->is_player)){
+    if (skill > 1 || !strcmp(type_user, "Cazador"))
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (entities[i] && !(entities[i]->is_player))
+            {
                 target = entities[i];
                 printf("Se usó abilidad %i contra %s\n", skill, target->type);
                 return target;
@@ -149,34 +192,40 @@ entity* select_target(int client_socket, entity** entities, int skill, char* typ
         }
     }
     // Si hay elección le pregunto
-    else {
+    else
+    {
         printf("Eligiendo target\n");
         display_players_targets(client_socket, entities);
         char *msg = server_receive_payload(client_socket);
         int player = atoi(msg);
         free(msg);
         int c = 0;
-        for(int i=0; i<5; i++){
-            if(entities[i] && entities[i]->is_player && player==c){
+        for (int i = 0; i < 5; i++)
+        {
+            if (entities[i] && entities[i]->is_player && player == c)
+            {
                 target = entities[i];
                 printf("Se usó abilidad %i contra %s\n", skill, target->type);
                 return target;
             }
-            c+=1;
+            c += 1;
         }
     }
     return NULL;
 }
 // Envía los jugadores en la party enumerados
-void display_players_targets(int client_socket, entity** entities){
+void display_players_targets(int client_socket, entity **entities)
+{
     char msg[100];
     char info[50];
     int contador = 0;
-    for(int i=0; i<5; i++){
-        if(entities[i] && entities[i]->is_player){
+    for (int i = 0; i < 5; i++)
+    {
+        if (entities[i] && entities[i]->is_player)
+        {
             sprintf(info, "%i) %s\n", contador, entities[i]->jugador->nombre);
             strcat(msg, info);
-            contador+=1;
+            contador += 1;
         }
     }
     server_send_message(client_socket, 78, msg);
@@ -191,7 +240,6 @@ void display_players_targets(int client_socket, entity** entities){
 //     actualize_entity(target);
 //     //TODO: Se termina el turno
 
-    
 // }
 
 // void actualize_entity(entity* player)
@@ -207,7 +255,7 @@ void display_players_targets(int client_socket, entity** entities){
 //             {
 //                 player->jugador->intoxicated = false;
 //                 player->dmg_per_turn -= 500; // TODO: Cambiar por variable cuando solucione el tema de importar variables globales
-//             } 
+//             }
 //         }
 //     }
 // }
